7c038da90709e226aef9af8978c07763d8c230a4
Govind <govind@Govinds-MacBook-Air.local>
2022-11-10 23:23:08 +0530

    fw21_1170_day-3

---
+++ sales/components/sales-nav.js
@@ -0,0 +1,15 @@
+function sales_nav(){
+    return `<div id="sales_menu">
+    <img id="sales_logo" src="https://cdn5.engagebay.com/images/256x256.png" alt="">
+    <div><a href="../home/home.html">Sales</a><span><i class="fa-solid fa-chevron-down"></i></span></div>
+
+    <div>Contacts<span><i class="fa-solid fa-chevron-down"></i></span></div>
+    <div><a href="/sales/sales_deal/sales_deal.html">Deals</a></div>
+    <div><a href="../sales_tasks/sales_tasks.html">Tasks</a></div>
+    <div>Automations</div>
+    <div><a href="../sales_calendar/sales-cal.html">Calendar</a></div>
+    <div><i class="fa-sharp fa-solid fa-ellipsis"></i></div>
+</div>`
+}
+
+export {sales_nav}
---
+++ sales/home/home.js
@@ -0,0 +1,4 @@
+import { sales_nav } from "../components/sales-nav.js";
+
+let navbar_div = document.getElementById('sales_navbar');
+navbar_div.innerHTML = sales_nav();
